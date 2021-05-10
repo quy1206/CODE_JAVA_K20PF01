64ac84469e95492da9eb9f53fd85d17880d3eab4
quy1206 <51883666+quy1206@users.noreply.github.com>
2021-05-10 11:30:06 +0700

    Thread starter 02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,86 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        new Thread (()->{
-        //Thread 1
-        for (int i = 0; i < 10; i++) {
-            System.out.println("a");
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        ArrayList<Integer> arrList = new ArrayList<>();
+        new Thread(()->{
+        for (int i = 0; i<10; i++){
+            arrList.add(new Random().nextInt(10));
         }
         }).start();
+        new Thread (()->{
+            for (int i = 0; i< arrList.size(); i++) {
+                System.out.println(arrList.get(i) + " ");
+            }
+        }).start();
+        new Thread (()->{
+            for (int i = 0; i< 10; i++) {
+                System.out.println(arrList.get(i) + " ");
+            }try {
+                System.out.println("t1");
+                Thread.sleep(500);
+            }catch (InterruptedException e ){
+                e.printStackTrace();
+            }
+        }).start();
 
-        new Thread(()->{
-            //Thread 2
-            for (int i = 0; i < 10; i++) {
-                System.out.println("b");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
+        //join
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i<10;i++){
+                    System.out.println(arrList.get(i) + " ");
+                }try {
+                    System.out.println("t1");
+                    Thread.sleep(1000);
+
+                }catch (InterruptedException e ){
                     e.printStackTrace();
                 }
             }
-        }).start();
-        }
+        });
 
+        t1.start();
+
+        try {
+            t1.join();
+        }catch (InterruptedException e ){
+            e.printStackTrace();
+        }
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i<10;i++){
+                    System.out.println(arrList.get(i) + " ");
+                }try {
+                    System.out.println("t2");
+                    Thread.sleep(1000);
+
+                }catch (InterruptedException e ){
+                    e.printStackTrace();
+                }
+            }
+        });
+        t2.start();
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i<10;i++){
+                    System.out.println(arrList.get(i) + " ");
+                }try {
+                    System.out.println("t3");
+                    Thread.sleep(1000);
+
+                }catch (InterruptedException e ){
+                    e.printStackTrace();
+                }
+            }
+        });
+        t3.start();
     }
+}
 
