5ed0088f25ab0a5bce69edee704fb4e557261b32
quy1206 <51883666+quy1206@users.noreply.github.com>
2021-05-10 09:49:47 +0700

    functon 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Function 1");
+        System.out.println("Function 2");
+
+    }
+}
