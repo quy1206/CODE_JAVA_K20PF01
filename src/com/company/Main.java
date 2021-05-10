a650cadca40a059c6d2bcb4917373ac9510f29af
quy1206 <51883666+quy1206@users.noreply.github.com>
2021-05-10 10:58:16 +0700

    Thread starter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Function 1");
-        System.out.println("Function 2");
-        System.out.println("Function 2");
 
+        new Thread (()->{
+        //Thread 1
+        for (int i = 0; i < 10; i++) {
+            System.out.println("a");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        }).start();
+
+        new Thread(()->{
+            //Thread 2
+            for (int i = 0; i < 10; i++) {
+                System.out.println("b");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        }
 
     }
-}
+
