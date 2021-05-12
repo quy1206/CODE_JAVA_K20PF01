2d01862f4c6ae193a6fdeb25a19f746b9013cb57
quy1206 <51883666+quy1206@users.noreply.github.com>
2021-05-12 09:35:58 +0700

    Thread starter 03 TEST

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,18 @@
+
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        WorkingThread obj =
-                new WorkingThread();
-        obj.start();
-
-        // declare
-        Working objRuna = new Working();
-        //
-        Thread obj1 = new Thread(objRuna);
-        obj1.start();
-
-        //new Thread
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10 ; i++) {
-                    System.out.println(i + " ");
-                    try {
-                        Thread.sleep(1000);//Alt + Enter
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-
-        //new Thread
+        Student obj = new Student();
         new Thread(()->{
-            for (int i = 0; i < 10 ; i++) {
-                System.out.println(i + " ");
-                try {
-                    Thread.sleep(1000);//Alt + Enter
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            obj.printPoint();
         }).start();
 
-
+        new Thread(()->{
+            obj.printPoint();
+        }).start();
     }
 }
